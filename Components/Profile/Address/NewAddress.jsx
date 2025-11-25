98317b2b6e87fcd6987dd18712ed17e2cc84009e
Baset <112559243+BasetMamshpoor@users.noreply.github.com>
2025-11-25 15:33:47 +0330

    Update NewAddress.jsx

--- Components/Profile/Address/NewAddress.jsx
+++ Components/Profile/Address/NewAddress.jsx
@@ -209,7 +209,7 @@ const NewAddress = ({edit, first, reload}) => {
                 <Edit/>
             </Button>}
 
-            <Modal classNames={{backdrop: "z-[1001]", wrapper: "z-[1002]"}} scrollBehavior="inside" isOpen={isOpen}
+            <Modal classNames={{backdrop: "z-[1001]", wrapper: "z-[1002]"}} scrollBehavior="inside" isOpen={isOpen} placment="center"
                    onOpenChange={onOpenChange} size="5xl">
                 <ModalContent>
                     {(onClose) => (
